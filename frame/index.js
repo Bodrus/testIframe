e03f1f4fe666c55bd68685d7b44ed1a71e4efe4f
Maxim.Bodrus <maxshop2010@gmail.com>
2019-03-03 18:24:41 +0200

    refactor(choice style after  changeWindow)

--- frame/index.js
+++ frame/index.js
@@ -1,36 +1,38 @@
 
-const changeWindow = () => {
-   const div = document.querySelector('div');
-   div.style.backgroundRepeat = 'no-repeat';
-   div.style.backgroundSize = 'auto';
-   
-   div.style.backgroundPosition = 'center center';
-   div.style.backgroundAttachment = 'fichangeWindowet';
-   
-   const { type } = screen.orientation;
- 
-   const w = window.innerWidth;
-   const h = window.innerHeight;
-   div.style.width = `${w}px`;
-   div.style.height = `${h}px`;
-  
-   if (type === 'portrait-primary') {
-      div.style.backgroundImage = 'url(portrait.png)';
-      div.style.backgroundColor = '#40bb63';
-      
-   } else if (type === 'landscape-primary') {
-      div.style.backgroundImage = 'url(landscape.png)';
-      div.style.backgroundColor = '#3b68cc'; 
-   }
+const createStyle = () => {
+   const body = document.querySelector('body');
+   body.style.backgroundRepeat = 'no-repeat';
+   body.style.backgroundSize = 'auto';
+   body.style.backgroundPosition = 'center center';
+   body.style.backgroundAttachment = 'fichangeWindowet';
 };
 
-const listener = (e) => {
+const choiсeStyle = {
+   'portrait-primary': { colour: '#40bb63', image: 'url(portrait.png)' },
+   'landscape-primary': { colour: '#3b68cc', image: 'url(landscape.png)' },
+};
+
+const changeWindow = () => {
+   const body = document.querySelector('body');
+   const { type } = screen.orientation;
+   const w = window.innerWidth;
+   const h = window.innerHeight;
+   body.style.width = `${w}px`;
+   body.style.height = `${h}px`;
+
+   const newStyleObj = choiсeStyle[type];
+   body.style.backgroundImage = newStyleObj.image;
+   body.style.backgroundColor = newStyleObj.colour;
+};
+
+const listener = () => {
    changeWindow();
 };
 
 window.addEventListener('load', () => {
    top.postMessage('Фраим загружен', "*");
    window.addEventListener("message", listener, false);
+   createStyle();
    changeWindow();
 });
 
